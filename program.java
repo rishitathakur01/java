b819832da447ca1410354878732f9dad8da7a8ce
rishitathakur01 <rishitat353@gmail.com>
2026-08-26 20:41:50 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,29 @@
+public class program {
+
+    public static void main(String[] args) {
+
+        
+        
+        int age = 20;
+        long studentId = 202460851L;
+        float attendance = 98.2f;
+        double cgpa = 9.2;
+        char grade = 'A';
+        boolean enrolled = true;
+        byte semester = 5;
+        short rollNumber = 1;
+       
+
+        System.out.println("===== STUDENT PROFILE =====");
+        System.out.println("Semester: " + semester);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Age: " + age);
+        System.out.println("Student ID: " + studentId);
+        System.out.println("Attendance: " + attendance + "%");
+        System.out.println("CGPA: " + cgpa);
+        System.out.println("Grade: " + grade);
+        System.out.println("Enrolled: " + enrolled);
+        System.out.println( Byte.MAX_VALUE);
+        System.out.println( Byte.MIN_VALUE);
+    }
+}
